6b3b8266bfea3b7da80cfbf42d7cb408cf5a813e
parkercornbrooks <pcorn123@gmail.com>
2024-12-03 12:47:27 -0500

    day2 input

---
+++ day2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	part := flag.Int("part", 1, "which part of the given day to run")
+	flag.Parse()
+
+	switch *part {
+	case 1:
+		fmt.Println("not yet implemented")
+	case 2:
+		fmt.Println("not yet implemented")
+	default:
+		fmt.Printf("part %d does not exist", *part)
+	}
+}
