b1a8736ba5197e3d69b18ae171733dc0f645295d
parkercornbrooks <pcorn123@gmail.com>
2024-12-03 12:01:34 -0500

    day 1 input and utils

---
+++ utils/utils.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"bufio"
+	"os"
+)
+
+type parseLine func(string)
+
+type endRead func()
+
+func ReadFile(filename string, linefn parseLine, endfn endRead) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	sc := bufio.NewScanner(file)
+	for sc.Scan() {
+		linefn(sc.Text())
+	}
+	if err := sc.Err(); err != nil {
+		panic(err)
+	}
+	endfn()
+}
