cd0ba413cc07a6d652760fe10f4ad75437a357a4
parkercornbrooks <pcorn123@gmail.com>
2024-12-05 15:28:02 -0500

    add test suite

--- README.md
+++ README.md
@@ -24,9 +24,22 @@ $ go run ./day2 -f "example.txt"
 
 ## Notes
 
+### Utils
+
 The `utils` package contains a `ReadInput()` function with 3 params:
 - filename - the default is the input file, which should be saved under the given day and named `input.txt` (e.g. `dayX/input.txt`). These files have been .gitignored as is standard for AoC. Alternatively it will read the passed `-f` flag under the same directory (e.g. `dayX/myOtherFile.txt`)
 - linefn - a function to run based on each line that is read. This is where the input file is read into whatever data format is necessary for the given puzzle. 
 - endfn - a function to run after the input file has been fully read, usually the end calculation
 
 I have found this format to be effective for abstracting the reading of the file while providing flexibility for different input structures and problem statements.
+
+### Testing
+
+Added a test suite so that if I change the utils I can confirm that the earlier runs still function as expected.
+
+To use it, run either:
+
+```sh
+$ go test
+$ go test -v
+```
---
+++ all_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+var testCases = []struct {
+	day  int
+	part int
+	want string
+}{
+	{
+		day:  1,
+		part: 1,
+		want: "Total: 2742123",
+	},
+	{
+		day:  1,
+		part: 2,
+		want: "Total: 21328497",
+	},
+	{
+		day:  2,
+		part: 1,
+		want: "Total safe reports: 306",
+	},
+	{
+		day:  2,
+		part: 2,
+		want: "Total safe reports: 366",
+	},
+	{
+		day:  3,
+		part: 1,
+		want: "Total: 178538786",
+	},
+	{
+		day:  3,
+		part: 2,
+		want: "Total: 102467299",
+	},
+	{
+		day:  4,
+		part: 1,
+		want: "2603",
+	},
+	{
+		day:  4,
+		part: 2,
+		want: "1965",
+	},
+}
+
+func TestAll(t *testing.T) {
+	for _, tc := range testCases {
+		name := fmt.Sprintf("Day %d Part %d", tc.day, tc.part)
+		daycmd := fmt.Sprintf("./day%d", tc.day)
+		partcmd := fmt.Sprintf("%d", tc.part)
+		t.Run(name, func(t *testing.T) {
+			cmd := exec.Command("go", "run", daycmd, "-p", partcmd)
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			result := strings.Split(string(out), "\n")[1]
+			if tc.want != result {
+				t.Errorf("Expected %q, got %q instead\n", tc.want, result)
+			}
+		})
+	}
+}
