3b7374d995ce202a5e9599224bdb526624bdea86
parkercornbrooks <pcorn123@gmail.com>
2024-12-20 17:31:25 -0500

    day15 setup

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/parkercornbrooks/advent-of-code-2024/day12"
 	"github.com/parkercornbrooks/advent-of-code-2024/day13"
 	"github.com/parkercornbrooks/advent-of-code-2024/day14"
+	"github.com/parkercornbrooks/advent-of-code-2024/day15"
 	"github.com/parkercornbrooks/advent-of-code-2024/day2"
 	"github.com/parkercornbrooks/advent-of-code-2024/day3"
 	"github.com/parkercornbrooks/advent-of-code-2024/day4"
@@ -40,6 +41,7 @@ var packageMap = map[int]dayPackage{
 	12: day12.New(),
 	13: day13.New(),
 	14: day14.New(),
+	15: day15.New(),
 }
 
 func main() {
